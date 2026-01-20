322b078d7f3f31442d5cee00e28e8a2cb8be668b
Sreeja Khan <voicenote8lifeline@gmail.com>
2026-01-20 22:32:43 +0530

    git docker file

--- journalApp/src/main/java/com/sreeja/journalApp/repository/JournalEntryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sreeja.journalApp.repository;
-
-import com.sreeja.journalApp.entity.JournalEntry;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface JournalEntryRepository extends MongoRepository<JournalEntry, String> {
-    // You can add custom query methods here if needed
-}
